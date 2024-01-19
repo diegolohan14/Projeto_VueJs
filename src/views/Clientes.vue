a5e6dc4ec085e1b35f8ec3279dc9831b2870aab8
diegolohan14 <diegolohan14@gmail.com>
2024-01-19 19:45:11 -0300

    Adicionando Registro de Clientes

---
+++ src/views/Clientes.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="Box">
+       <h1>Cadastro de Cliente</h1>
+       <form @submit.prevent="handleSubmit" class="formulario">
+         <div>
+           <label for="name">Nome</label>
+           <input type="text" id="name" v-model="customer.name" required>
+         </div>
+         <div>
+           <label for="document">Documento</label>
+           <input type="text" id="document" v-model="customer.document" required>
+         </div>
+         <div>
+           <label for="phone">Telefone</label>
+           <input type="text" id="phone" v-model="customer.phone" required>
+         </div>
+         <div>
+           <label for="email">Email</label>
+           <input type="email" id="email" v-model="customer.email" required>
+         </div>
+         <div>
+           <label for="active">Ativo</label>
+           <select id="active" v-model="customer.active">
+             <option value="true">Sim</option>
+             <option value="false">Não</option>
+           </select>
+         </div>
+         <button type="submit">Enviar</button>
+       </form>
+    </div>
+   </template>
+   
+   <script>
+   export default {
+    data() {
+       return {
+         customer: {
+           name: '',
+           document: '',
+           phone: '',
+           email: '',
+           active: true
+         }
+       }
+    },
+    methods: {
+       handleSubmit() {
+       }
+    }
+   };
+   </script>
--- src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-</script>
-
-<template>
-  <h1 class="text-3xl font-bold underline text-red-700">
-    Hello world!
-  </h1>
-</template>
